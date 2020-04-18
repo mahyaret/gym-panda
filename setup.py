5c9c846f1043829f361dfd8b0384df204a7d5da9
Monster <mahyar.etedal@icloud.com>
2020-04-18 15:38:37 -0400

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from setuptools import setup
+import setuptools
 from pathlib import Path
 
-setup(name='gym_panda',
-      version='0.0.1',
-      long_description=Path("README.md").read_text(),
-      install_requires=['gym', 'pybullet', 'numpy']  # And any other dependencies foo needs
+setuptools.setup(
+    name='gym_panda',
+    author="Mahyar Abdeetedal",
+    author_email="mahyar.etedal@icloud.com",
+    version='0.0.3',
+    description="A OpenAI Gym Env for Panda",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/mahyaret/gym-panda",
+    packages=setuptools.find_packages(include="gym_panda*"),
+    install_requires=['gym', 'pybullet', 'numpy'],  # And any other dependencies foo needs
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.6'
 )
