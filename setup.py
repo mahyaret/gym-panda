a1f05540284af6b34a594c501062410a3d7f6ccf
Monster <mahyar.etedal@icloud.com>
2020-04-18 09:47:25 -0400

    add req

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from pathlib import Path
 setup(name='gym_panda',
       version='0.0.1',
       long_description=Path("README.md").read_text(),
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym', 'pybullet', 'numpy']  # And any other dependencies foo needs
 )
